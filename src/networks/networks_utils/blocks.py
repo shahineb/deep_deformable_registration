e6b71823f46bc06d61f40c6f752aae8530c44c3c
vatre <trean.v@gmail.com>
2018-12-06 11:06:24 +0100

    Fixed mutable default arguments and pep8 stuff

--- src/networks/networks_utils/blocks.py
+++ src/networks/networks_utils/blocks.py
@@ -7,26 +7,22 @@ class Block(object):
     __metaclass__ = ABCMeta
 
     @staticmethod
-    def get_input_dimension(input):
+    def get_input_dimension(inp):
         """Retrieves dimension of the data
 
         Args:
-            input (keras.layer)
+            inp (keras.layer)
 
         Returns:
             ndims (int): Dimension of the layer in [1, 2, 3]
 
         """
-        ndims = len(input.get_shape()) - 2
+        ndims = len(inp.get_shape()) - 2
         assert ndims in [1, 2, 3], "ndims should be one of 1, 2, or 3. found: %d" % ndims
         return ndims
 
     @abstractmethod
-    def __init__(self):
-        pass
-
-    @abstractmethod
-    def build(self, input):
+    def build(self, inp):
         pass
 
 
@@ -41,12 +37,14 @@ class ConvBlock(Block):
         norm_kwargs (dict): parameters for normalization
     """
 
-    def __init__(self,
-                 activation="",
-                 normalize=False,
-                 conv_kwargs={},
-                 activation_kwargs={},
-                 norm_kwargs={}):
+    def __init__(self, activation="", normalize=False, conv_kwargs=None,
+                 activation_kwargs=None, norm_kwargs=None):
+        if conv_kwargs is None:
+            conv_kwargs = {}
+        if activation_kwargs is None:
+            activation_kwargs = {}
+        if norm_kwargs is None:
+            norm_kwargs = {}
         self.activation_ = activation
         self.normalize_ = normalize
         self.conv_kwargs_ = conv_kwargs
@@ -83,15 +81,15 @@ class SqueezeExciteBlock(Block):
     def __init__(self, ratio):
         self.ratio_ = ratio
 
-    def build(self, input):
-        ndims = Block.get_input_dimension(input)
+    def build(self, inp):
+        ndims = Block.get_input_dimension(inp)
 
         channel_axis = 1 if K.image_data_format() == "channels_first" else -1
-        filters = input._keras_shape[channel_axis]
+        filters = inp._keras_shape[channel_axis]
         se_shape = tuple([1] * ndims) + (filters,)
-        globalAveragePooling = getattr(KL, 'GlobalAveragePooling%dD' % ndims)
+        global_average_pooling = getattr(KL, 'GlobalAveragePooling%dD' % ndims)
 
-        se = globalAveragePooling()(input)
+        se = global_average_pooling()(inp)
         se = KL.Reshape(se_shape)(se)
         se = KL.Dense(filters // self.ratio_, activation='relu', kernel_initializer='he_normal', use_bias=False)(se)
         se = KL.Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(se)
@@ -100,5 +98,5 @@ class SqueezeExciteBlock(Block):
             permutation = (ndims,) + tuple(range(1, ndims))
             se = KL.Permute(permutation)(se)
 
-        x = KL.multiply([input, se])
+        x = KL.multiply([inp, se])
         return x
